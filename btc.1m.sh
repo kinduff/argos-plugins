f040c197a7001d0ec08c003805ad7013ebbefe89
kinduff <kinduff@protonmail.com>
2022-04-22 12:13:28 +0200

    Uses 2 decimal places for BTC

--- btc.1m.sh
+++ btc.1m.sh
@@ -5,4 +5,4 @@
 # <bitbar.author.github>kinduff</bitbar.author.github>
 # <bitbar.version>1</bitbar.version>
 
-echo "💎 $(curl -s https://api.coinbase.com/v2/exchange-rates?currency=BTC | jq -r '.data.rates.EUR')"
+echo "$(curl -s https://api.coinbase.com/v2/exchange-rates?currency=BTC | jq -r '.data.rates.EUR')" | xargs printf "💎 %.2f"
